05dfb89e0087d49970f296df59cb490a42571b8d
jihenboughalmi40 <jihenboughalmip4@gmail.com>
2021-07-22 12:59:15 +0200

    mon atelier

---
+++ ajoutEmployé.php
@@ -0,0 +1,30 @@
+<?php
+echo "<table border=1>
+<tr>
+<td>Cin</td>
+<td>Nom</td>
+<td>Prenom</td>
+<td>Nb heures</td>
+<td>Tarif horaires</td>
+</tr>";
+echo "<tr><td>" .$_POST['cin']. "</td>";
+echo "<td>" .$_POST['nom']. "</td>"; 
+echo "<td>" .$_POST['prenom']. "</td>";
+echo "<td>" .$_POST['nb heures']. "</td>";
+ echo " <td>" .$_POST['tarif horaire'] . "</td></tr>";
+"</table>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
